76974c3cc365f75faf9cc637530105d625a959ad
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-01-09 11:20:58 +0900

    feat: 파일 업로드 리턴 타입 추가

---
+++ src/types/Uploads.ts
@@ -0,0 +1,10 @@
+export interface UploadFileInfo {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  destination: string;
+  filename: string;
+  path: string;
+  size: number;
+}
