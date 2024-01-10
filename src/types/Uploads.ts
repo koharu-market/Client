34a63ba00590d1326bc87792e39ed9cf96ec47c9
seokmin <msm0748@gmail.com>
2024-01-10 10:34:07 +0900

    refactor: 업로드 파일 타입 수정

--- src/types/Uploads.ts
+++ src/types/Uploads.ts
@@ -1,10 +1,4 @@
 export interface UploadFileInfo {
-  fieldname: string;
   originalname: string;
-  encoding: string;
-  mimetype: string;
-  destination: string;
-  filename: string;
   path: string;
-  size: number;
 }
