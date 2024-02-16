bc19c5fe3854b97129d3df2ff534445c42c10861
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-02-16 19:55:35 +0900

    refactor: 카테고리 API axios 수정

--- src/lib/getCategories.ts
+++ src/lib/getCategories.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import { axiosInstance } from './axios';
 
 export async function getCategories() {
   try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URI}/category`);
+    const response = await axiosInstance.get('/category');
     const categories = response.data;
     return categories;
   } catch (error) {
