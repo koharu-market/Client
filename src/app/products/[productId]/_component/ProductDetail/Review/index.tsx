3ff939a3722c51befbcbc34b9ef6cfc1f63f88e2
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-02-19 00:27:03 +0900

    feat: 상품 페이지 리뷰 구현중

--- src/app/products/[productId]/_component/ProductDetail/Review/index.tsx
+++ src/app/products/[productId]/_component/ProductDetail/Review/index.tsx
@@ -1,7 +1,11 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import CreateReview from './CreateReview';
+import Score from '@/components/ui/Score';
+import { axiosInstance } from '@/lib/axios';
+import { Review as IReview } from '@/types/Review';
+import ReviewFeed from './ReviewFeed';
 
 interface Props {
   productId: number;
@@ -10,8 +14,17 @@ interface Props {
 }
 
 export default function Review({ productId, score, reviewCount }: Props) {
-  const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState<IReview[]>([]);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const getReview = async () => {
+      const response = await axiosInstance.get(`/review/${productId}`);
+      const data = response.data;
+      setReviews(data);
+    };
+    getReview();
+  }, [productId]);
 
   const openModal = useCallback(() => {
     setIsOpen(true);
@@ -33,6 +46,25 @@ export default function Review({ productId, score, reviewCount }: Props) {
           </button>
         </div>
         <CreateReview isOpen={isOpen} closeModal={closeModal} productId={productId} />
+      </div>
+      <div className="mt-5">
+        <div className="bg-primary-100 rounded-md md:py-12 py-8 flex text-slate-500 text-sm">
+          <div className="flex md:flex-row flex-col items-center md:gap-5 gap-0 md:w-1/2 w-[136px] justify-center">
+            <div className="md:order-1 order-2 md:scale-100 scale-75">
+              <Score score={score} size="base" />
+            </div>
+            <span className="md:order-2 order-1 font-bold md:text-[34px] text-2xl text-[#424242] mt-2">
+              {score.toFixed(1)}
+            </span>
+          </div>
+          <div className="border-l-[1px] border-l-slate-300 flex justify-center flex-1">
+            <div>
+              {[5, 4, 3, 2, 1].map(index => (
+                <ReviewFeed key={index} targetScore={index} reviews={reviews} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
